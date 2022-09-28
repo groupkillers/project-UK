ca3f25dcc2f1d70f3ca0bc31c5e69e16e050d882
Thinu <88131481+Rameshthinushan@users.noreply.github.com>
2022-09-29 00:10:33 +0530

    thinu login success

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<Register/>
+	<router-view/>
 </template>
 
 <script>
@@ -16,7 +16,6 @@
 			Register
 		},	
 		created () {
-			
 			/* axios
 				.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 				.then(response => (this.info = response)) */
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,26 @@
 // import './bootstrap';
 
-import {createApp} from 'vue'
-
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
+import Signup from './Register.vue'
+import Signin from './Signin.vue'
+import Home from './Home.vue'
+import Dashboard from './Dashboard.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/signup', component: Signup },
+  { path: '/signin', component: Signin },
+  { path: '/dashboard', component: Dashboard },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
 const app = createApp(App)
+app.use(router)
 app.mount("#app")
