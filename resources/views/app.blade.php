817c45f2aae721e1eb860935b53e357f1d64f66f
Thinu <88131481+Rameshthinushan@users.noreply.github.com>
2022-10-09 23:57:35 +0530

    final commit for thinu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,29 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>How To Install Vue 3 in Laravel 9 with Vite</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	<link 
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" 
+		rel="stylesheet" 
+		integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" 
+		crossorigin="anonymous"
+	/>
+
+	<!-- Font Awesome -->
+	<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+		rel="stylesheet"
+	/>
+	<!-- Google Fonts -->
+	<link
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+		rel="stylesheet"
+	/>
+	<!-- MDB -->
+	<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
+		rel="stylesheet"
+	/>
+
 
 	@vite('resources/css/app.css')
 </head>
